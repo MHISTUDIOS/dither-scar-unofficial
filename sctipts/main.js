f44400e84989d315776e677d05e99c59175cf63a
FlareGhost (MHI) <mhistudios228@gmail.com>
2025-07-03 18:23:54 +0200

    finalizing website
    почти всё готово 🥳. Осталось добавить плейсхолдер и всё готово. Надеюсь ему зайдёт)

---
+++ sctipts/main.js
@@ -0,0 +1,32 @@
+
+
+
+//========== ESSENTIAL CARDS ==========
+//============ AND OVERALL ============
+//============ ANIM LOGIC =============
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.card', { delay: 200 });
+//=====================================
+
+
+
+//#####################################
+//#          HOMEPAGE PART            #
+//#   it also works for other tabs    #
+//#####################################
+
+//============ MAIN PART ==============
+ScrollReveal().reveal('.logo', { delay: 200 });
+ScrollReveal().reveal('.main-btns', { delay: 200 });
+ScrollReveal().reveal('.main-text', { delay: 200 });
+//=====================================
+
+//============ SPICY PART =============
+ScrollReveal().reveal('.spicy-video', { delay: 200 });
+ScrollReveal().reveal('.spicy-text', { delay: 200 });
+//=====================================
+
+//========== INTERFACE PART ===========
+ScrollReveal().reveal('.interface-video', { delay: 200 });
+ScrollReveal().reveal('.interface-text', { delay: 200 });
+//=====================================
