476efc466ffeea70da6f7c9c466babad21b7e44d
FlareGhost (MHI) <mhistudios228@gmail.com>
2025-07-03 18:48:16 +0200

    added preloader
    теперь вроде всё

--- sctipts/main.js
+++ sctipts/main.js
@@ -1,4 +1,12 @@
-
+window.addEventListener('load', function () {
+    let preloader = document.querySelector('.preloader');
+    window.setTimeout(() => {
+        preloader.style.opacity = 0;
+    }, 1000)
+    window.setTimeout(() => {
+        preloader.style.display = 'none';
+    }, 3000)
+})
 
 
 //========== ESSENTIAL CARDS ==========
